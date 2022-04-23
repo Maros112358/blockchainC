fff7f2fda170297a0ac166cd3abe0ca4857f4d58
Maros Bratko <maros112358@marosb.marosb>
2022-04-23 22:32:22 +0200

    change name

--- main.c
+++ main.c
@@ -4,8 +4,8 @@
 int main() {
     Blockchain* bc = createBlockChain();
     
-    AddBlock(bc, "Send 1 BTC to Ivan");
-    AddBlock(bc, "Send 2 more BTC to Ivan");
+    AddBlock(bc, "Send 1 BTC to Stepan");
+    AddBlock(bc, "Send 2 more BTC to Stepan");
 
     for (int i = 0; i < bc -> length; i++) {
         printf("Prev. hash: %x\n", bc -> blocks[i] -> prevBlockHash);
